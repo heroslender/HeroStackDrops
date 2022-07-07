7acc68ac190989c83d4045cf611a2a28a8d42aeb
heroslender <admin@heroslender.com>
2022-07-07 20:45:51 +0100

    Bump jetbrains annotations from 22.0.0 to 23.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
         compileOnly("org.projectlombok:lombok:1.18.24")
         annotationProcessor("org.projectlombok:lombok:1.18.24")
 
-        compileOnly("org.jetbrains:annotations:22.0.0")
+        compileOnly("org.jetbrains:annotations:23.0.0")
     }
 
     java {
